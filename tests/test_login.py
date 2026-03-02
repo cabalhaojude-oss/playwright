e5fa98be17d2264a6b0a3c511ceca0b396b8a094
Jude Cabalhao <cabalhaojude@gmail.com>
2026-03-02 10:22:01 +0800

    Add rerun failures

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,17 +7,14 @@ from utils.test_data import VALID_USER, INVALID_USERNAME, INVALID_PASSWORD
 def test_valid_login(login_page, page):
     login_page.login(VALID_USER["username"], VALID_USER["password"])
     expect(page.get_by_text("Logged In Successfully")).to_be_visible()
-    # page.screenshot(path= "success_login_screenshot.png")
 
 @pytest.mark.invalid_login
 def test_invalid_username(login_page, page):
     login_page.login(INVALID_USERNAME["username"], INVALID_USERNAME["password"])
-    expect(page.locator("#error")).to_contain_text("Your is invalid!")
-    # page.screenshot(path= "invalid_username_screenshot.png")
+    expect(page.locator("#error")).to_contain_text("Your username is invalid!")
 
 @pytest.mark.invalid_login
 def test_invalid_password(login_page, page):
     login_page.login(INVALID_PASSWORD["username"], INVALID_PASSWORD["password"])
     expect(page.locator("#error")).to_contain_text("Your password is invalid!")
-    # page.screenshot(path="invalid_username_screenshot.png")
 
